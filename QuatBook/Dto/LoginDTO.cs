aae013be10af480159bc984eaf0a02c365959ea1
dat-2k4 <datnthe173076@fpt.edu.vn>
2025-05-24 21:25:44 +0700

    1
    abc

--- QuatBook/Dto/LoginDTO.cs
+++ QuatBook/Dto/LoginDTO.cs
@@ -7,7 +7,7 @@ namespace QuatBook.Dto
         [Required(ErrorMessage = "Chưa nhập tên đăng nhập")]
         [MaxLength(20, ErrorMessage = "Tối đa 20 kí tự")]
         public string Username { get; set; }
-
+//hello
         [Required(ErrorMessage = "Chưa nhập mật khẩu")]
         [DataType(DataType.Password)]
         public string Password { get; set; }
